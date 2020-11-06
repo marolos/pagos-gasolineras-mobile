50ea59bf802dd95721c4ba244fb0abbb5a9897dd
miguelquo <miguelps95@gmail.com>
2020-11-06 15:38:38 -0500

    add user search result for transfer

--- components/transfer/TransfersNavigator.js
+++ components/transfer/TransfersNavigator.js
@@ -8,7 +8,6 @@ import Label from '../shared/Label';
 import TransfersView from './TransfersView';
 import tailwind from 'tailwind-rn';
 import CreateTransferView from './CreateTransferView';
-import SelectReceiver from './SelectReceiver';
 
 const Stack = createStackNavigator();
 
@@ -36,15 +35,6 @@ export default function TransfersNavigator(props) {
          <Stack.Screen
             name="createTransferView"
             component={CreateTransferView}
-            options={() => ({
-               headerTitle: () => (
-                  <Label text={'Transferir saldo'} style={tailwind('text-base mt-1')} focused />
-               ),
-            })}
-         />
-			<Stack.Screen
-            name="selectReciverUser"
-            component={SelectReceiver}
             options={() => ({
                headerTitle: () => (
                   <Label text={'Transferir saldo'} style={tailwind('text-base mt-1')} focused />
